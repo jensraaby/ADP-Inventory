0927447c4cef2d933cf73b807970e83db48d5c67
Jens Raaby (adp) <jens.raaby@adpeople.com>
2013-03-11 17:00:15 +0100

    added basic models

--- inventoryapp/tracker/models.py
+++ inventoryapp/tracker/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class Device(models.Model):
-    """
-    Device: parent class for all devices
-    """
-    # give the device's serial number
-    uuid = models.CharField(max_length=100)
-    added_date = models.DateTimeField('date added')
-    
 class DeviceManufacturer(models.Model):
     name = models.CharField(max_length=50)
     added_date = models.DateTimeField('date added')
@@ -19,6 +11,38 @@ class DeviceModel(models.Model):
     model_name = models.CharField(max_length=50)
     manufacturer = models.ForeignKey(DeviceManufacturer)
     added_date = models.DateTimeField('date added')
+    
+class Device(models.Model):
+    """
+    Device: parent class for all devices
+    """
+    # give the device's serial number
+    uuid = models.CharField(max_length=100)
+    added_date = models.DateTimeField('date added')
+    device_model = models.ForeignKey(DeviceModel)
+    
+    class Meta:
+           abstract = True
+ 
+ 
+class MobileDevice(Device):
+    """
+    Mobile is a phone, modem or anything with a sim-card
+    """
+    imei = models.CharField(max_length=50)
+    iccid = models.CharField(max_length=50)  
+           
+           
+class WorkstationDevice(Device):
+    """
+    Workstation is a computer (mac or windows)
+    """
+    os = models.CharField(max_length=100)
+    dns_name = models.CharField(max_length=100)
+       
+
+    
+
     
 
     
