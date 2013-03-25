8d46a885a997603b0154656faabf155b70323ad8
Jens Raaby (adp) <jens.raaby@adpeople.com>
2013-03-25 13:49:49 +0100

    created clean rails app

--- config.sh
+++
@@ -1,5 +0,0 @@
-workon adp-it
-pip install docutils
-pip install django
-pip install psycopg2
-pip install python-ldap
--- trackit/devices/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from devices.models import DeviceRegistration, Workstation, Mobile
-
-admin.site.register(DeviceRegistration)
-admin.site.register(Workstation)
-admin.site.register(Mobile)
