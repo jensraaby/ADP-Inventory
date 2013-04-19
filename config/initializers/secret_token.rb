11b0d5cc12d89403593aecb5ee103e7c43c4e3da
Jens Raaby (adp) <jens.raaby@adpeople.com>
2013-04-19 11:10:51 +0200

    Re-creating rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ADPInventory::Application.config.secret_token = '01d0628aeccb8e91a40a35c3f7efbd8ee83ec9946dc598e2b4cb8476f6ddeb0ecfe88cad4b54dc7d1a2d47086131dfe2e615031482c70e81a3b6ee0e377331f5'
