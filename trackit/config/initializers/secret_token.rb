c337d202d10e219a88cc01e31e55c9137686daa2
Jens Raaby (adp) <jens.raaby@adpeople.com>
2013-04-19 11:23:18 +0200

    shiny new rails app

---
+++ trackit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trackit::Application.config.secret_token = 'fc148359fdd6202b178b0b43bee25b60e5fce6494ba65471f6444e989652f3be7edad5d09d1bd2759260f99f26f6a4c76212066a5c31392c6e90c2d216cede4e'
