e6b4e903ec4fe01325340b3f48f0abb6c0858d3f
Jens Raaby <jens+github@m.raaby.co.uk>
2013-04-23 19:33:03 +0300

    Update user.rb
    added some comments to user model

--- trackit/app/models/user.rb
+++ trackit/app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :active, :displayName, :givenName, :mail, :sAMAccountName, :sn, :telephoneNumber, :userPrincipalName
 
+  # you can call user.registrations to get an array of all ther registrations
   has_many :registrations
   
+  # every update is tracked with paper_trail gem. including destroy
   has_paper_trail
+  
+  # functionality should go in a controller, not here! Separation of concerns :)
 end
