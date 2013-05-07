3e6968651d376ac42ed105b9e2b5ac8dbaf5b1b7
jensraaby <jens+github@m.raaby.co.uk>
2013-05-07 14:05:05 +0200

    couple of changes to capfile

--- trackit/config/deploy.rb
+++ trackit/config/deploy.rb
@@ -1,13 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "bundler/capistrano" 
+load 'deploy/assets'
 
+set :hostname, "152.146.38.56"
+set :application, "trackit"
+set :repository,  "https://github.com/jensraaby/ADP-Inventory.git"
+set :user, "xadmin"
+set :deploy_to, "/Volumes/MacSupportMirror/TrackIT/deploy"
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, hostname                        # Your HTTP server, Apache/etc
+role :app, hostname                          # This may be the same as your `Web` server
+role :db,  hostname, :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+set :rails_env, :production
+set :branch, 'master'
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -16,10 +24,17 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+  
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
